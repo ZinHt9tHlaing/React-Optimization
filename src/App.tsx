7814e54587110806d5746a715b9c10757f5db0a1
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-10-14 11:05:11 +0630

    Component Splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,25 @@
 import "./App.css";
 
-import Title from "./Title";
-import Counter from "./Counter";
+import Counter from "./components/Counter";
 import { useState } from "react";
+import Title from "./components/Title";
+import InitialSetup from "./components/InitialSetup";
 
 const App = () => {
   console.log("App rendered");
 
   const [initialValue, setInitialValue] = useState<number>(0);
-  const [inputValue, setInputValue] = useState<string>("");
 
-  const handleSubmit = () => {
-    console.log("handleSubmit function called");
+  const onSetupHandler = (inputValue: number) => {
+    console.log("onSetupHandler in App - function called");
 
-    const numericValue = Number(inputValue);
-    if (!isNaN(numericValue)) {
-      setInitialValue(numericValue);
-    } else {
-      console.log("Pleaser enter a valid number.");
-    }
+    setInitialValue(inputValue);
   };
 
   return (
     <>
       <Title />
-      <h2>Initial Value Setup</h2>
-      <input
-        type="number"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Enter initial value"
-      />
-      <button onClick={handleSubmit}>Submit</button>
+      <InitialSetup onSetup={onSetupHandler} />
       <Counter initialValue={initialValue} />
     </>
   );
---
+++ src/components/InitialSetup.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+// Props and types for InitialSetup
+interface InitialSetupProps {
+  onSetup: (value: number) => void;
+}
+
+const InitialSetup = ({ onSetup }: InitialSetupProps) => {
+  console.log("InitialSetup rendered");
+
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handleSubmit = () => {
+    console.log("handleSubmit function called");
+
+    const numericValue = Number(inputValue);
+    if (!isNaN(numericValue)) {
+      onSetup(numericValue);
+      setInputValue("");
+    } else {
+      console.log("Pleaser enter a valid number.");
+    }
+  };
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <>
+      <h2>Initial Value Setup</h2>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={handleOnChange}
+        placeholder="Enter initial value"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </>
+  );
+};
+
+export default InitialSetup;
