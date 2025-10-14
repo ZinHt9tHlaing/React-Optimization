3f7dfe17d93a737641187316f050ce869ca81366
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-10-14 11:46:44 +0630

    React memo

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,9 @@
+import { memo } from "react";
+
 const Title = () => {
   console.log("Title rendered");
 
   return <h1>Optimization: memo, useMemo, useCallback</h1>;
 };
 
-export default Title;
+export default memo(Title);
